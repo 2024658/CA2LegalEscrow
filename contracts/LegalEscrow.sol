9a685c148a4834ecb615da62549847a683f83c52
ngesh <2024658@student.cct.ie>
2026-01-04 01:11:15 +0000

    deposit function
    Added deposit function for escrow funding

--- contracts/LegalEscrow.sol
+++ contracts/LegalEscrow.sol
@@ -12,4 +12,11 @@ contract LegalEscrow {
         solicitor = _solicitor;
     }
 
+    // Client deposits funds into escrow
+    function deposit() external payable {
+        require(msg.sender == client, "Only client can deposit");
+        require(msg.value > 0, "Deposit must be greater than zero");
+        amount = msg.value;
+    }
+
 }
